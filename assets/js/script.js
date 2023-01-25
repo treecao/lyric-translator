429cfb0d4966c5ea9cd03bbdea3363ecd718eece
treecao <94645628+treecao@users.noreply.github.com>
2023-01-24 23:25:53 -0600

    editing out JS & commenting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,31 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'c6f85fcc6bmsh14916e6daadafd9p1db2b9jsnf63ac98ea77f',
-		'X-RapidAPI-Host': 'songmeanings.p.rapidapi.com'
-	}
-};
+// commenting out all JS for now -- WIP
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'c6f85fcc6bmsh14916e6daadafd9p1db2b9jsnf63ac98ea77f',
+// 		'X-RapidAPI-Host': 'songmeanings.p.rapidapi.com'
+// 	}
+// };
 
-fetch('https://songmeanings.p.rapidapi.com/?key=c6f85fcc6bmsh14916e6daadafd9p1db2b9jsnf63ac98ea77f&sm_lid=0&lyric_title=promise&artist_name=romeo%20santos&format=json&method=lyrics.get', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-
-	const selectTag = document.querySelectorAll("select");
-
-selectTag.forEach(selectTag => {
-	console.log(selectTag);
-});
+// fetch('https://songmeanings.p.rapidapi.com/?key=c6f85fcc6bmsh14916e6daadafd9p1db2b9jsnf63ac98ea77f&sm_lid=0&lyric_title=promise&artist_name=romeo%20santos&format=json&method=lyrics.get', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
 
 
+// 	const selectTag = document.querySelectorAll("select");
 
+// selectTag.forEach(selectTag => {
+// 	console.log(selectTag);
+// });
 
 
 
-let apiKey = 'c6f85fcc6bmsh14916e6daadafd9p1db2b9jsnf63ac98ea77f'
 
-function getApi(){
-	var requestUrl = "google-translate1.p.rapidapi.com"
-}
+
+
+// let apiKey = 'c6f85fcc6bmsh14916e6daadafd9p1db2b9jsnf63ac98ea77f'
+
+// function getApi(){
+// 	var requestUrl = "google-translate1.p.rapidapi.com"
+// }
