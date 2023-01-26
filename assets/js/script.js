06bf782d2187d2c99b5d8bc4954a86bfdcb418f3
treecao <94645628+treecao@users.noreply.github.com>
2023-01-25 21:53:20 -0600

    adding lyric api code --awaiting server from Mo

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,37 +2,40 @@
 let apiKey = "154536b7fb7d5ecc50beb74e5cb895a8";
 let fetchButton = document.querySelector("#searchBtn")
 
-// function getLyric(songName) {
-//     // let requestUrl = `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${songName}&apikey=${apiKey}`;
-//     // &q_artist=${artist}
-//     let requestUrl = `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=sexy%20and%20i%20know%20it&q_artist=lmfao
-//     &apikey=${apiKey}`;}
-   
-    var myHeaders = new Headers();
-    myHeaders.append("apikey", "154536b7fb7d5ecc50beb74e5cb895a8");
-    
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow', mode:"no-cors"
-    };
-    
-    fetch("http://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=save%20me&q_artist=avenged%20sevenfold&apikey=154536b7fb7d5ecc50beb74e5cb895a8", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
+function getLyric(songName) {
+    // let requestUrl = `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${songName}&apikey=${apiKey}`;
+    // &q_artist=${artist}
+    let requestUrl = `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=sexy%20and%20i%20know%20it&q_artist=lmfao
+    &apikey=${apiKey}`;
 
-//     fetch(requestUrl, {
-//         mode:"no-cors"
-//     })
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         // console.log(data)
-// }
+    fetch(requestUrl, {
+        mode:"no-cors"
+    })
+        .then(function (response) {
+            console.log(response)
+            return response.json;
+        })
+        .then(console.log)
+        .catch(console.error)
+}
 
-// fetchButton.addEventListener('click', function(){
-//     const songTitle = document.querySelector(".songTitle");
-//     let song = songTitle.value;
-//     getLyric(song)
-// });
+fetchButton.addEventListener('click', function(){
+    const songTitle = document.querySelector(".songTitle");
+    let song = songTitle.value;
+    
+});
+getLyric()
+
+// var myHeaders = new Headers();
+// myHeaders.append("apikey", "154536b7fb7d5ecc50beb74e5cb895a8");
+
+// var requestOptions = {
+//   method: 'GET',
+//   headers: myHeaders,
+//   redirect: 'follow', mode:"no-cors"
+// };
+
+// fetch("http://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=save%20me&q_artist=avenged%20sevenfold&apikey=154536b7fb7d5ecc50beb74e5cb895a8", requestOptions)
+//   .then(response => response.text())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
