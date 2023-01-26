b1782edcd029f8400c35e4a359be3952bce6f467
treecao <94645628+treecao@users.noreply.github.com>
2023-01-25 20:07:50 -0600

    adding api lyric code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,38 @@
-// commenting out all JS for now -- WIP
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'c6f85fcc6bmsh14916e6daadafd9p1db2b9jsnf63ac98ea77f',
-// 		'X-RapidAPI-Host': 'songmeanings.p.rapidapi.com'
-// 	}
-// };
+// beginning of lyric api integration
+let apiKey = "154536b7fb7d5ecc50beb74e5cb895a8";
+let fetchButton = document.querySelector("#searchBtn")
 
-// fetch('https://songmeanings.p.rapidapi.com/?key=c6f85fcc6bmsh14916e6daadafd9p1db2b9jsnf63ac98ea77f&sm_lid=0&lyric_title=promise&artist_name=romeo%20santos&format=json&method=lyrics.get', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
+// function getLyric(songName) {
+//     // let requestUrl = `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${songName}&apikey=${apiKey}`;
+//     // &q_artist=${artist}
+//     let requestUrl = `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=sexy%20and%20i%20know%20it&q_artist=lmfao
+//     &apikey=${apiKey}`;}
+   
+    var myHeaders = new Headers();
+    myHeaders.append("apikey", "154536b7fb7d5ecc50beb74e5cb895a8");
+    
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow', mode:"no-cors"
+    };
+    
+    fetch("http://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=save%20me&q_artist=avenged%20sevenfold&apikey=154536b7fb7d5ecc50beb74e5cb895a8", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
 
-
-// 	const selectTag = document.querySelectorAll("select");
-
-// selectTag.forEach(selectTag => {
-// 	console.log(selectTag);
-// });
-
-
-
-
-
-
-// let apiKey = 'c6f85fcc6bmsh14916e6daadafd9p1db2b9jsnf63ac98ea77f'
-
-// function getApi(){
-// 	var requestUrl = "google-translate1.p.rapidapi.com"
+//     fetch(requestUrl, {
+//         mode:"no-cors"
+//     })
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         // console.log(data)
 // }
+
+// fetchButton.addEventListener('click', function(){
+//     const songTitle = document.querySelector(".songTitle");
+//     let song = songTitle.value;
+//     getLyric(song)
+// });
