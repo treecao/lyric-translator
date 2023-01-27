8f5934dc870355e5a2e41b0bfb6b0d1e32ea5586
xAsdfaf <georgejordanjr8@gmail.com>
2023-01-26 21:48:51 -0600

    added functional api integration

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,11 @@
 // // client id - SqLiLKN6DJazosWp9rDJGFMgnI8vdP4Gv_NuyII3yv71Uys3BHPSjbkNve9iy0Bb
 
 // beginning of lyric api integration
-
-let lyricApiKey = "154536b7fb7d5ecc50beb74e5cb895a8";
-
-
+const song = document.querySelector(".description");
+const lyrics = document.createElement("p")
 
 function songLyric() {
   // 
-  
-
-  var lyrics = document.querySelector(".description").innerHTML; //= body.lyrics.lyrics_body[0].translatedText
-  console.log(lyrics);
   var songName = document.getElementById("songTitle").value;
   var artistName = document.getElementById("artist-name").value;
   var url = "https://93b9b720-f4e9-47a8-9f08-341808be2c12.trayapp.io/matcher.lyrics.get?q_track=" + songName + "&q_artist=" + artistName + "&apikey=154536b7fb7d5ecc50beb74e5cb895a8";
@@ -23,14 +17,18 @@ function songLyric() {
 
   fetch(url)
   .then(response => response.json())
-  .then(result => console.log(result))
+  .then((result) => {
+    let songLyrics = result.message.body.lyrics.lyrics_body
+    console.log(songLyrics.split("*******")[0])
+    lyrics.textContent = songLyrics.split("*******")[0]
+    song.appendChild(lyrics);
+    console.log(result.message.body.lyrics.lyrics_body)
+  })
   .catch(error => console.log('error', error));
 };
 
-function getApi() {
+$("#search-song").on("click", songLyric)
 
-};
-// $("button").on("click", getApi);
 
 // translator api code begins here
 let fetchButton = document.querySelector("#searchBtn")
@@ -38,7 +36,7 @@ let fetchButton = document.querySelector("#searchBtn")
 let apiKey = "AIzaSyDvziRZmcbKKXHY2F8MOmbO68tHiSo0fMY"
 const translateBtn = document.querySelector(".buttonT")
 const selectTag = document.querySelectorAll("select");
-const song = document.querySelector(".description");
+
 
 selectTag.forEach(selectTag => { 
     for (const country_code in countries) {
