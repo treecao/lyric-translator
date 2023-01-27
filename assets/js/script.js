0c921ccf05dccaf0093967b902b8792ba8867279
xAsdfaf <georgejordanjr8@gmail.com>
2023-01-26 21:01:32 -0600

    integrated lyric api

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,81 +2,40 @@
 // // client secret - 6TN71dOvz3RALlGNElYVxN8fcWs9EugrZmw6BlgP7yBPFA9hSGhH2DEAKuAFWRKFVUXlG_872zKAYznnWDbr6Q
 // // client id - SqLiLKN6DJazosWp9rDJGFMgnI8vdP4Gv_NuyII3yv71Uys3BHPSjbkNve9iy0Bb
 
+// beginning of lyric api integration
+
+let lyricApiKey = "154536b7fb7d5ecc50beb74e5cb895a8";
 
 
-// let apiKey = "154536b7fb7d5ecc50beb74e5cb895a8";
-// function getApi() {
-// 	var requestUrl = "http://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=sexy%20and%20i%20know%20it&q_artist=lmfao&apikey=154536b7fb7d5ecc50beb74e5cb895a8";
-// 	fetch(requestUrl, {
-// 		mode: "no-cors" 
-// 	})
 
-// 	.then(function(response) {
-// 		console.log(response.json);
-// 	})
-// }
-function getApi(){
-  fetch("https://93b9b720-f4e9-47a8-9f08-341808be2c12.trayapp.io/matcher.lyrics.get?q_track=save%20me&q_artist=avenged%20sevenfold&apikey=154536b7fb7d5ecc50beb74e5cb895a8")
-  .then(response => response.text())
+function songLyric() {
+  // 
+  
+
+  var lyrics = document.querySelector(".description").innerHTML; //= body.lyrics.lyrics_body[0].translatedText
+  console.log(lyrics);
+  var songName = document.getElementById("songTitle").value;
+  var artistName = document.getElementById("artist-name").value;
+  var url = "https://93b9b720-f4e9-47a8-9f08-341808be2c12.trayapp.io/matcher.lyrics.get?q_track=" + songName + "&q_artist=" + artistName + "&apikey=154536b7fb7d5ecc50beb74e5cb895a8";
+  console.log(songName);
+  console.log(artistName);
+  console.log(url);
+
+  fetch(url)
+  .then(response => response.json())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
-  $("description")
 };
 
-$("button").on("click", getApi);
+function getApi() {
 
-let lyricApiKey = "AIzaSyDvziRZmcbKKXHY2F8MOmbO68tHiSo0fMY"
+};
+// $("button").on("click", getApi);
 
-
-
-
-
-
-
-
-
-// beginning of lyric api integration
-let apiKey = "154536b7fb7d5ecc50beb74e5cb895a8";
+// translator api code begins here
 let fetchButton = document.querySelector("#searchBtn")
 
-function getLyric(songName) {
-    // let requestUrl = `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${songName}&apikey=${apiKey}`;
-    // &q_artist=${artist}
-    let requestUrl = `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=sexy%20and%20i%20know%20it&q_artist=lmfao
-    &apikey=${apiKey}`;
-
-    fetch(requestUrl, {
-        mode:"no-cors"
-    })
-        .then(function (response) {
-            console.log(response)
-            return response.json;
-        })
-        .then(console.log)
-        .catch(console.error)
-}
-
-fetchButton.addEventListener('click', function(){
-    const songTitle = document.querySelector(".songTitle");
-    let song = songTitle.value;
-    
-});
-getLyric()
-
-
-//commenting out full JS file --WIP
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'c6f85fcc6bmsh14916e6daadafd9p1db2b9jsnf63ac98ea77f',
-// 		'X-RapidAPI-Host': 'songmeanings.p.rapidapi.com'
-// 	}
-// };
-
-// fetch('https://songmeanings.p.rapidapi.com/?key=c6f85fcc6bmsh14916e6daadafd9p1db2b9jsnf63ac98ea77f&sm_lid=0&lyric_title=promise&artist_name=romeo%20santos&format=json&method=lyrics.get', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
+let apiKey = "AIzaSyDvziRZmcbKKXHY2F8MOmbO68tHiSo0fMY"
 const translateBtn = document.querySelector(".buttonT")
 const selectTag = document.querySelectorAll("select");
 const song = document.querySelector(".description");
@@ -101,35 +60,9 @@ function translateText() {
     fetch(url)
     .then(res => res.json())
     .then(data => {
-
-// var myHeaders = new Headers();
-// myHeaders.append("apikey", "154536b7fb7d5ecc50beb74e5cb895a8");
-
-// var requestOptions = {
-//   method: 'GET',
-//   headers: myHeaders,
-//   redirect: 'follow', mode:"no-cors"
-// };
-
-
-// fetch("http://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=save%20me&q_artist=avenged%20sevenfold&apikey=154536b7fb7d5ecc50beb74e5cb895a8", requestOptions)
-//   .then(response => response.text())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
-
-
         document.querySelector('.textTranslation').innerText = data.data.translations[0].translatedText
     })
 }
 
 
 translateBtn.addEventListener("click", translateText);
-
-
-
-// let apiKey = 'c6f85fcc6bmsh14916e6daadafd9p1db2b9jsnf63ac98ea77f'
-
-// function getApi(){
-// 	var requestUrl = "google-translate1.p.rapidapi.com"
-// }
-
